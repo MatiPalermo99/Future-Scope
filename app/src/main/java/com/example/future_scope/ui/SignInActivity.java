aeefda0f2836294d9157e38ade82045a5331eaef
micapierotti <micapierotti@live.com.ar>
2021-02-11 00:03:33 -0300

    Auth Firebase - Falta: logout, guardar user, email y user unicos

--- app/src/main/java/com/example/future_scope/ui/SignInActivity.java
+++ app/src/main/java/com/example/future_scope/ui/SignInActivity.java
@@ -3,8 +3,12 @@ package com.example.future_scope.ui;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -12,15 +16,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.future_scope.MainActivity;
 import com.example.future_scope.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
 
     private EditText emailInicio,contraseniaInicio;
     private Button registrarse,iniciarSesion;
     int count = 0;
+    private FirebaseAuth fAuth;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -35,6 +45,8 @@ public class SignInActivity extends AppCompatActivity {
         registrarse=findViewById(R.id.inicio_sesion_a_registro);
         iniciarSesion=findViewById(R.id.aceptar_inicio);
 
+        fAuth = FirebaseAuth.getInstance();
+
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,8 +58,36 @@ public class SignInActivity extends AppCompatActivity {
         iniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(SignInActivity.this, MainActivity.class);
-                startActivity(i);
+                String email = emailInicio.getText().toString().trim();
+                String password = contraseniaInicio.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    emailInicio.setError("Email requerido.");
+                    return;
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    emailInicio.setError("Email inválido.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)) {
+                    contraseniaInicio.setError("Contraseña requerida.");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignInActivity.this, "¡Bienvenidos al Himalaya!", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(SignInActivity.this, MainActivity.class);
+                            startActivity(i);
+                        }else{
+                            Toast.makeText(SignInActivity.this, "Error: "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
             }
         });
     }
--- app/src/main/java/com/example/future_scope/ui/SignUpActivity.java
+++ app/src/main/java/com/example/future_scope/ui/SignUpActivity.java
@@ -3,6 +3,8 @@ package com.example.future_scope.ui;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -10,16 +12,23 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.future_scope.MainActivity;
 import com.example.future_scope.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText emailRegistro,usuarioRegistro,contraseniaRegistro,repetirContraseniaRegistro;
     private Button registrarse;
+    private FirebaseAuth fAuth;
 
     @SuppressLint({"ClickableViewAccessibility", "WrongViewCast"})
     @Override
@@ -35,11 +44,60 @@ public class SignUpActivity extends AppCompatActivity {
         repetirContraseniaRegistro = findViewById(R.id.repetir_contrasenia_registro);
         registrarse=findViewById(R.id.aceptar_registro);
 
+        fAuth = FirebaseAuth.getInstance();
+        if(fAuth.getCurrentUser() != null){
+            Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
+
         registrarse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(SignUpActivity.this, SignInActivity.class);
-                startActivity(i);
+                String email = emailRegistro.getText().toString().trim();
+                String password = contraseniaRegistro.getText().toString().trim();
+                String confirmPassword = repetirContraseniaRegistro.getText().toString().trim();
+                String user = usuarioRegistro.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    emailRegistro.setError("Email obligatorio.");
+                    return;
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    emailRegistro.setError("Email inválido.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)) {
+                    contraseniaRegistro.setError("Contraseña obligatoria.");
+                    return;
+                }else if(password.length() < 8 || password.length() > 20){
+                    contraseniaRegistro.setError("Debe tener entre 8 y 20 caracteres");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(user)){
+                    usuarioRegistro.setError("Usuario obligatorio.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(confirmPassword) || confirmPassword.compareTo(password) != 0){
+                    repetirContraseniaRegistro.setError("Las contraseñas deben coincidir.");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this, "Usuario creado con éxito", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+                            startActivity(i);
+                        }else{
+                            Toast.makeText(SignUpActivity.this, "Error: "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
     }
